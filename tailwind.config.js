fcec05870bc6c80485f1ab086810ddf019c997a3
Valeriy0 <v.kistrinov@yandex.ru>
2025-03-14 16:20:08 +0300

    update conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   
   theme: {
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
     extend: {
       colors: {
         mainBg: "#080808",
